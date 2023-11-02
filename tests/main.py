f98462318ba57764664b2089083ad137934f568f
Shin_Kungoo <88479555+shinkungoo@users.noreply.github.com>
2023-11-02 20:11:01 +0800

    Create files

---
+++ tests/main.py
@@ -0,0 +1,24 @@
+import wandb
+
+from pprint import pprint
+from inteledu import listener
+from inteledu.datahub import DataHub
+from inteledu.models.cs.neural import NCDM
+
+wandb.init(
+    project="test inteledu"
+)
+
+listener.update(wandb.log)
+
+datahub = DataHub("datasets/Math2")
+datahub.random_split()
+datahub.random_split(source="valid", to=["valid", "test"])
+print("Number of response logs {}".format(len(datahub)))
+
+ncdm = NCDM(datahub.student_num, datahub.exercise_num, datahub.knowledge_num)
+ncdm.build()
+ncdm.train(datahub, "train", "valid")
+test_results = ncdm.score(datahub, "test", metrics=["acc", "doa"])
+pprint(test_results)
+
