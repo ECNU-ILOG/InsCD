e575ad467415a687b41abb86bb5925b6cb3a4849
Shin_Kungoo <88479555+shinkungoo@users.noreply.github.com>
2023-11-07 22:53:42 +0800

    Update listener

--- inteledu/_listener.py
+++ inteledu/_listener.py
@@ -1,5 +1,5 @@
 class _Listener:
-    def __init__(self, collector=None):
+    def __init__(self):
         """
         Description:
         A singleton decorate type to collect valid data during training. Default it does work. If you want to listen to
@@ -9,12 +9,15 @@ class _Listener:
         Parameters:
         collector: a function can collect dict{} during model training.
         """
-        self.__collector = collector
+        self.__collector = print
 
     def update(self, collector):
         self.__collector = collector
 
     def reset(self):
+        self.__collector = print
+
+    def silence(self):
         self.__collector = None
 
     def __call__(self, func):
