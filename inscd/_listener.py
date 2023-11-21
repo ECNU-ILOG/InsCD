3913731ad10457d2b2873851f7d54b0b6a27f836
Mizuyusan <88479555+shinkungoo@users.noreply.github.com>
2023-11-21 22:24:29 +0800

    Improve listener to support format function.

--- inscd/_listener.py
+++ inscd/_listener.py
@@ -7,9 +7,15 @@ class _Listener:
         listener.update(wandb.log). If you want to stop listening, you can use listener.silence().
         """
         self.__collector = print
+        self.percentage = True
+        self.precision = 2
 
     def update(self, collector):
         self.__collector = collector
+
+    def set_format(self, percentage=True, precision=2):
+        self.percentage = percentage
+        self.precision = precision
 
     def reset(self):
         self.__collector = print
@@ -17,9 +23,14 @@ class _Listener:
     def silence(self):
         self.__collector = None
 
+    def format(self, result):
+        for key, value in result.items():
+            result[key] = round(value * 100, self.precision) if self.percentage else round(value, self.precision)
+        return result
+
     def __call__(self, func):
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
+            result = self.format(func(*args, **kwargs))
             if self.__collector is not None:
                 self.__collector(result)
             return result
